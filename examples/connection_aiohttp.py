9e1e26ea7533a8fea0baf2838dd23fc7c6824178
goo <mattkerschbaum@Hotmail.com>
2023-01-03 06:27:27 -0800

    Minor updates to refactor send command and also add a data request with each heartbeat

--- examples/connection_aiohttp.py
+++ examples/connection_aiohttp.py
@@ -6,32 +6,31 @@ from goopy_ibcp.clientportal_websockets import ClientPortalWebsocketsBase
 
 
 @logger.catch
-async def main():
+async def main_http():
     logger.add("testlog.log")
     client_http = ClientPortalHttpAio(watchdog_start=False)
     r = await client_http.clientrequest_validate()
     r = await client_http.clientrequest_user()
     r = await client_http.clientrequest_portfolio_accounts()
-    r = await client_http.clientrequest_authentication_status()
+    # r = await client_http.clientrequest_authentication_status()
     await client_http.clientrequest_server_accounts()
     await asyncio.sleep(5)
 
     while True:
-        await client_http.clientrequest_marketdata("495512551", "31")
-        await asyncio.sleep(1)
+        await client_http.clientrequest_marketdata("495512572", "31")
+        await client_http.clientrequest_validate()
+        await asyncio.sleep(5)
 
 
 @logger.catch
-def main_ws():
+def main_ws() -> None:
     logger.add("testlog.log")
+
     client_ws = ClientPortalWebsocketsBase()
     client_ws.loop()
 
 
 if __name__ == "__main__":
-    # Use this for the websocket demo
-    main_ws()
-
-    # Use this for the http demo
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main())
+    # event loop for the demos (websocket or http)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main_ws())
